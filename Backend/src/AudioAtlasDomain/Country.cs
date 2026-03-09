ce83472a8ff986855b81f64533dfd54190c712b2
annanassen <annar@itu.dk>
2026-03-09 21:46:26 +0100

    add Genre.cs and Country.cs

---
+++ Backend/src/AudioAtlasDomain/Country.cs
@@ -0,0 +1,9 @@
+namespace DefaultNamespace;
+
+public class Country
+{
+    public string Id { get; set ; }
+    public string Name { get; set ; }
+	public List<Genre> Genres { get; set; }
+	
+}
---
+++ Backend/src/AudioAtlasDomain/Genre.cs
@@ -0,0 +1,7 @@
+namespace DefaultNamespace;
+
+public class Genre
+{
+    public string Id { get; set }
+    public string Name { get; set }
+}
