e9d1bc31131228d2c03a57b2407203c8c97ebc66
Noav <noav@itu.dk>
2026-03-16 17:13:50 +0100

    Made configurations for tabels

--- Backend/src/AudioAtlasView/Program.cs
+++ Backend/src/AudioAtlasView/Program.cs
@@ -1,14 +1,25 @@
 using AudioAtlasInfrastructure.Database;
+using AudioAtlasInfrastructure.Database.Seed;
 using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var app = builder.Build();      
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    var seedLogger = scope.ServiceProvider.GetRequiredService<ILogger<DbInitializer>>();
+
+    app.Logger.LogInformation("Running database migration and seed.");
+    ctx.Database.Migrate();
+    DbInitializer.SeedDatabase(ctx, seedLogger);
+    app.Logger.LogInformation("Database migration and seed completed.");
+}
 
 app.Run();
