6e29be74ce55890573f28f793dedb501ac7f05a4
Noav <noav@itu.dk>
2026-03-16 13:44:09 +0100

    Removed merge conflicts

--- Backend/src/AudioAtlasView/Program.cs
+++ Backend/src/AudioAtlasView/Program.cs
@@ -3,31 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
-<<<<<<< HEAD
-=======
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-
->>>>>>> main
-var app = builder.Build();
-
-app.MapGet("/api/countries", () => "Heyerrbod!");
-
-app.MapGet("/api/countries/{id}/genres", () => "Hello World!");
-
-<<<<<<< HEAD
-app.MapGet("/api/genres/{id}", () => "Hello Girl!");
+var app = builder.Build();      
 
 app.Run();
-=======
-
-app.Run();
->>>>>>> main
