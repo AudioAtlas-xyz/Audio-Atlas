bcfee5f35cdc4e5bd68a30da248f7ff631805c26
Freja <frsj@itu.dk>
2026-03-17 09:03:44 +0100

    Add queries to CountryRepository

--- Backend/src/AudioAtlasApplication/Repositories/ICountryRepository.cs
+++ Backend/src/AudioAtlasApplication/Repositories/ICountryRepository.cs
@@ -1,9 +1,13 @@
 namespace AudioAtlasApplication.Repositories;
 
 using AudioAtlasDomain.Geography;
+using AudioAtlasDomain.Genres;
 
 public interface ICountryRepository
 { //hey errbod'
-    //public Country getCountryByID(string id);
+    public Country getCountryByID(string id);
+    public Dictionary<Country, int> getGenreCounts();
+
+    public ICollection<Genre> getGenres(Country country);
 
 }
