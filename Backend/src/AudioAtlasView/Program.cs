cc56c3c3e5605827717b6c20c9ece8f51d89dd5e
diahitu <diah@itu.dk>
2026-03-18 10:46:43 +0100

    Readme and openapi (#6)
    * added swagger
    
    * changed contributing and the readme files and added readme for backend as well
    
    * remove package that conflicts with new changes, remove package reference that should be unnecessary, commented out database LocalDb provider, since it only works on windows

--- Backend/src/AudioAtlasView/Program.cs
+++ Backend/src/AudioAtlasView/Program.cs
@@ -38,13 +38,50 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-    var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    /*var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     var seedLogger = scope.ServiceProvider.GetRequiredService<ILogger<DbInitializer>>();
 
     app.Logger.LogInformation("Running database migration and seed.");
-    ctx.Database.Migrate();
+    //ctx.Database.Migrate(); - does not work on Ubuntu :(
     DbInitializer.SeedDatabase(ctx, seedLogger);
     app.Logger.LogInformation("Database migration and seed completed.");
+    */
 }
 
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+app.MapGet("/", () => "Hello World");
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
 app.Run();
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
