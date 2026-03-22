78f7fb6e478a0a7438729f16c6dd2e5e78afe465
Noav <noav@itu.dk>
2026-03-22 21:05:21 +0100

    Removed DBCOntextDefaults, as connection string is gotten from appconfig now

--- Backend/src/AudioAtlasInfrastructure/Database/AppDbContextFactory.cs
+++ Backend/src/AudioAtlasInfrastructure/Database/AppDbContextFactory.cs
@@ -26,8 +26,6 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
 
-        optionsBuilder.UseSqlServer(AppDbContextDefaults.DevelopmentConnectionString);
-
         return new AppDbContext(optionsBuilder.Options);
     }
 }
