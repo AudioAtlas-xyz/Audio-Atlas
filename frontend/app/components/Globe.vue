aeca5a54295718734fecc068a437462f62ece32c
annanassen <annar@itu.dk>
2026-04-08 10:29:53 +0200

    Change colors and change to hexPolygon

--- frontend/app/components/Globe.vue
+++ frontend/app/components/Globe.vue
@@ -22,26 +22,34 @@ export default {
           const maxVal = Math.max(...countries.features.map(getVal));
           colorScale.domain([0, maxVal]);
 
+          const countryColors = new Map();
+          const baseHue = 120;
+          const range = 90;
 
+          countries.features.forEach((feat) => {
+            countryColors.set(
+              feat.properties.ISO_A2, `hsl(${ baseHue + Math.random() * range}, 67%, 50%)`
+            )
+          })
 
           const myGlobe = Globe()(globeDiv.value)
-             .globeImageUrl('https://raw.githubusercontent.com/annanassen/Ricart-Argawala/main/discoballtext.jpg')
+             .globeImageUrl('https://unpkg.com/three-globe@2.45.2/example/img/earth-night.jpg')
              //.bumpImageUrl('https://unpkg.com/three-globe@2.45.1/example/img/earth-topology.png')
-             .backgroundImageUrl('https://raw.githubusercontent.com/annanassen/Ricart-Argawala/main/partyscene.webp')
+             .backgroundImageUrl('https://unpkg.com/three-globe@2.45.2/example/img/night-sky.png')
              .lineHoverPrecision(0)
-             .polygonsData(countries.features.filter(d => d.properties.ISO_A2 !== 'AQ'))
-              .polygonAltitude(0.06)
-              .polygonCapColor(feat => colorScale(getVal(feat)))
-              .polygonSideColor(() => 'rgba(0, 100, 0, 0.15)')
-              .polygonStrokeColor(() => '#111')
-              .polygonLabel(({properties: d}) => `
+             .hexPolygonsData(countries.features.filter(d => d.properties.ISO_A2 !== 'AQ'))
+              .hexPolygonAltitude(0.06)
+              .hexPolygonResolution(3)
+              .hexPolygonMargin(0.2)
+              .hexPolygonUseDots(true)
+              .hexPolygonColor(feat => countryColors.get(feat.properties.ISO_A2))
+              .hexPolygonLabel(({properties: d}) => `
                 <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
-                GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
                 Population: <i>${d.POP_EST}</i>
               `)
-              .onPolygonHover(hoverD => myGlobe
-                .polygonAltitude(d => d === hoverD ? 0.12 : 0.06)
-                .polygonCapColor(d => d === hoverD ? 'steelblue' : colorScale(getVal(d)))
+              .onHexPolygonHover(hoverD => myGlobe
+                .hexPolygonAltitude(d => d === hoverD ? 0.12 : 0.06)
+                .hexPolygonColor(d => d === hoverD ? '#FF69B4' : countryColors.get(d.properties.ISO_A2))
               )
               .polygonsTransitionDuration(300);
 
