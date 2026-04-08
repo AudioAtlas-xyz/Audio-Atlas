40147656cfea595f27a817523319a5298aba3938
annanassen <annar@itu.dk>
2026-04-08 15:47:08 +0200

    change hexPolygon back to polygon for clearer country boundaries

--- frontend/app/components/Globe.vue
+++ frontend/app/components/Globe.vue
@@ -37,22 +37,21 @@ export default {
              //.bumpImageUrl('https://unpkg.com/three-globe@2.45.1/example/img/earth-topology.png')
              .backgroundImageUrl('https://unpkg.com/three-globe@2.45.2/example/img/night-sky.png')
              .lineHoverPrecision(0)
-             .hexPolygonsData(countries.features.filter(d => d.properties.ISO_A2 !== 'AQ'))
-              .hexPolygonAltitude(0.06)
-              .hexPolygonResolution(3)
-              .hexPolygonMargin(0.2)
-              .hexPolygonUseDots(true)
-              .hexPolygonColor(feat => countryColors.get(feat.properties.ISO_A2))
-              .hexPolygonLabel(({properties: d}) => `
+             .polygonsData(countries.features.filter(d => d.properties.ISO_A2 !== 'AQ'))
+              .polygonAltitude(0.06)
+              .polygonCapColor(feat => countryColors.get(feat.properties.ISO_A2))
+              .polygonSideColor(() => 'rgba(0, 100, 0, 0.15)')
+              .polygonStrokeColor(() => '#111')
+              .polygonLabel(({properties: d}) => `
                 <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
                 Population: <i>${d.POP_EST}</i>
               `)
-              .onHexPolygonHover(hoverD => myGlobe
-                .hexPolygonAltitude(d => d === hoverD ? 0.12 : 0.06)
-                .hexPolygonColor(d => d === hoverD ? '#FF69B4' : countryColors.get(d.properties.ISO_A2))
+              .onPolygonHover(hoverD => myGlobe
+                .polygonAltitude(d => d === hoverD ? 0.12 : 0.06)
+                .polygonCapColor(d => d === hoverD ? '#FF69B4' : countryColors.get(d.properties.ISO_A2))
               )
               .polygonsTransitionDuration(300)
-              .onHexPolygonClick(({ properties: d }) => {
+              .onPolygonClick(({ properties: d }) => {
               console.log('Clicked country:', d.ADMIN);
               //needs to be a panel
               alert(`You clicked on ${d.ADMIN}`);
