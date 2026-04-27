434dbee796e707e670fadc47a9cc9499c17d3e3b
Camilla <cfjo@itu.dk>
2026-04-27 16:11:13 +0200

    Added components to submission form identity page Co-authored-by: annanassen <annar@itu.dk> Co-authored-by: frxsj <frsj@itu.dk>

--- frontend/app/pages/submission-form.vue
+++ frontend/app/pages/submission-form.vue
@@ -1,17 +1,41 @@
 <script setup lang="ts">
 import type { Country } from '~/types/country'
+import type {Genre} from "~/types/genre";
+
+const { data } = await useFetch<Country[]>('api/countries/all')
+
+const countryNames = computed(() =>
+  data.value?.countries?.map(c => ({
+    label: c.name,
+    value: c.name
+  })) ?? []
+)
 
 const form = useTemplateRef('form')
 </script>
 
 <template>
   <Stepper></Stepper>
-  <div :class="$style.genreidentityformcard"
-       margin-left="auto"
-       margin-right="auto">
+  <div :class="$style.genreidentityformcard">
     <UForm ref="form" />
-      <h1>bleepbloop</h1>
-    <SelectMenu></SelectMenu>
+    <UContainer style="padding: 3rem;">
+    <div :class="$style.identity">Identity</div>
+    <h1>Name this genre and tell us where it comes from.</h1>
+
+
+      <UFormField label="Genre name" required>
+        <UInput placeholder="e.g Afrobeats" style="color: #7a84a8" class="w-full"/>
+      </UFormField>
+
+      <UFormField label="Aliases" hint="Optional">
+        <UInput placeholder="Add an alias and press enter" class="w-full"></UInput>
+      </UFormField>
+
+      <UFormField label="COUNTRY / COUNTRIES OF ORIGIN" field="name" required>
+        <SubmissionSelectMenu :itemList ="countryNames" class="w-full"/>
+      </UFormField>
+
+    </UContainer>
   </div>
 </template>
 
@@ -19,7 +43,6 @@ const form = useTemplateRef('form')
 .genreidentityformcard {
   width: 50%;
   height: 37.5rem;
-  position: absolute;
   border-radius: 6px;
   background-color: #0d0f1a;
   border: 1px solid #1c2038;
@@ -27,9 +50,28 @@ const form = useTemplateRef('form')
   overflow: hidden;
   text-align: left;
   font-size: 0.75rem;
-  color: #e4e8f5;
+  color: #7a84a8;
   font-family: Space_Grotesk;
-  margin-left: auto;
-  margin-right: auto;
+  margin: 2rem auto 0 auto;
+}
+.identity {
+  width: 4.813rem;
+  position: relative;
+  font-size: 1.25rem;
+  font-weight: 500;
+  font-family: 'Space Grotesk';
+  color: #e4e8f5;
+  text-align: left;
+  display: inline-block;
+}
+
+h1 {
+  color: #7a84a8;
+}
+UFormField{
+  width: 100%;
+}
+UInput{
+  width: 100%;
 }
 </style>
