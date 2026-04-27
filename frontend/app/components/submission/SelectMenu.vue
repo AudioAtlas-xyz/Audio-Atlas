fc89c7fe58e77966e3b4655538b1d6091c39158e
Camilla <cfjo@itu.dk>
2026-04-27 12:21:35 +0200

    add selectmenu component Co-authored-by: annanassen <annar@itu.dk> Co-authored-by: frxsj <frsj@itu.dk>

---
+++ frontend/app/components/submission/SelectMenu.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactive, useTemplateRef } from 'vue'
+import * as z from 'zod'
+import type { FormSubmitEvent } from '@nuxt/ui'
+
+const props =
+
+const items = ref(['Backlog', 'Todo', 'In Progress', 'Done'])
+const value = ref('Backlog')
+
+
+const schema = z.object({
+  selectMenuMultiple: z.any().refine(values => !!values?.find((option: any) => option.value === 'option-2'), {
+    message: 'Option 2 must be included'
+  })
+})
+
+
+</script>
+
+<template>
+  <USelectMenu v-model="value" :items="items" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/app/pages/submission-form.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const form = useTemplateRef('form')
+</script>
+
+<template>
+  <UForm ref="form" />
+  <Stepper></Stepper>
+  <UCard>
+    <h1>bleepbloop<h1>
+  </UCard>
+  <SelectMenu></SelectMenu>
+</template>
+
+<style scoped>
+
+</style>
