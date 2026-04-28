c091894c53c2af48c62cf98b92f578c4d5bc1789
annanassen <annar@itu.dk>
2026-04-28 14:03:01 +0200

    add button and refine layout in submission form page
    Co-authored-by: frxsj <frsj@itu.dk>
    Co-authored-by: cfjo <cfjo@itu.dk>

--- frontend/app/pages/submission-form.vue
+++ frontend/app/pages/submission-form.vue
@@ -10,6 +10,10 @@ const state = reactive({
   countries: []
 })
 
+function nextStep() {
+  currentStep.value++
+}
+
 const countryNames = computed(() =>
   data.value?.map(c => ({
     label: c.name,
@@ -17,34 +21,53 @@ const countryNames = computed(() =>
   })) ?? []
 )
 
-const form = useTemplateRef('form' )
+const currentStep = ref(1)
+
 </script>
 
 <template>
-  <Stepper :current-state="step" />
-  <div :class="$style.genreidentityformcard">
-    <UForm ref="form" :state="state" />
-    <UContainer style="padding: 3rem;">
-    <div :class="$style.identity">Identity</div>
-    <h1>Name this genre and tell us where it comes from.</h1>
+  <div v-if="currentStep === 1">
+  <Stepper :current-state="currentStep" />
+    <div :class="$style.genreidentityformcard">
+      <UContainer style="padding: 3rem;">
+      <div :class="$style.identity">Identity</div>
+      <h1>Name this genre and tell us where it comes from.</h1>
+
+        <div :class="$style.formFields">
+          <UFormField label="GENRE NAME" required>
+            <UInput placeholder="e.g Afrobeats" class="w-full"/>
+            <h1> Name of the genre. </h1>
+          </UFormField>
 
 
-      <UFormField label="Genre name" required>
-        <UInput placeholder="e.g Afrobeats" style="color: #7a84a8" class="w-full"/>
-      </UFormField>
+          <UFormField label="ALIASES" hint="(Optional)">
+            <UInput placeholder="Add an alias and press enter" class="w-full"></UInput>
+            <h1> Alternative names, transliterations or regional names. </h1>
+          </UFormField>
 
-      <UFormField label="Aliases" hint="Optional">
-        <UInput placeholder="Add an alias and press enter" class="w-full"></UInput>
-      </UFormField>
 
-      <UFormField label="COUNTRY / COUNTRIES OF ORIGIN" field="name" required>
-        <SubmissionSelectMenu
-          v-model="state.countries"
-          :itemList ="countryNames"
-          class="w-full"/>
-      </UFormField>
+          <UFormField label="COUNTRY / COUNTRIES OF ORIGIN" field="name" required>
+            <SubmissionSelectMenu
+              v-model="state.countries"
+              :itemList ="countryNames"
+              class="w-full"/>
+            <h1> Select all countries where this genre originated - not just where it became popular. </h1>
+          </UFormField>
+        </div>
+        <div :class="$style.buttonRow" style="color: #3de8c8">
+          <UButton @click="nextStep">
+            Next: About
+            <UIcon name="i-heroicons-arrow-right-20-solid" />
+          </UButton>
+        </div>
 
-    </UContainer>
+      </UContainer>
+    </div>
+  </div>
+
+  <div v-if="currentStep === 2">
+    <Stepper :current-state="currentStep" />
+
   </div>
 </template>
 
@@ -77,10 +100,18 @@ const form = useTemplateRef('form' )
 h1 {
   color: #7a84a8;
 }
-UFormField{
-  width: 100%;
+
+.formFields {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;  /* Adjust this value for more/less space */
+  margin-top: 1.5rem;
 }
-UInput{
-  width: 100%;
+
+.buttonRow {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 7rem;
 }
+
 </style>
