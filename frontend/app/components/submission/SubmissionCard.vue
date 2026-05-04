7565c44054a92b30da33ea06492ff961d3aa2a50
Camilla <cfjo@itu.dk>
2026-05-04 13:44:07 +0200

    add sumbission queue page and submission card component Co-authored-by: annanassen <annar@itu.dk>

---
+++ frontend/app/components/submission/SubmissionCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+
+</script>
+
+
+<template>
+  <UPageCard
+    v-bind="item"
+    :variant="index % 2 === 0 ? 'soft' : 'outline'"
+    class="rounded-none text-white"
+
+
+  />
+</template>
+
+
+<style scoped>
+
+
+</style>
+
---
+++ frontend/app/pages/pending-submissions.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const items = Array.from({ length: 30 }, (_, i) => ({
+  id: i + 1,
+  title: `Item ${i + 1}`,
+  description: `Description for item ${i + 1}`
+}))
+</script>
+
+
+<template>
+  <UContainer class="max-w-5xl mx-auto">
+    <h1>Pending Submissions</h1>
+    <UScrollArea
+      v-slot="{ item, index }"
+      :items="items"
+      class="w-full h-96"
+    >
+      <UPageCard
+        v-bind="item"
+        :variant="index % 2 === 0 ? 'soft' : 'outline'"
+        class="rounded-none text-white"
+
+
+      />
+    </UScrollArea>
+  </UContainer>
+
+
+</template>
+
+
+<style scoped>
+h1{
+  margin: 2rem;
+  color: white;
+  font-size: 3.5rem;
+  font-weight: 850;
+  line-height: 0.88;
+}
+</style>
