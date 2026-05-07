6105ae761ec203ec52582432e7ff42a8c6a465c1
annanassen <annar@itu.dk>
2026-05-07 16:58:13 +0200

    add server: false to fix pending submissions page and delete mock data

--- frontend/app/pages/pending-submissions.vue
+++ frontend/app/pages/pending-submissions.vue
@@ -10,50 +10,13 @@ const { data: countriesData } = await useFetch<Country[]>('/api/countries/all')
 const { data: genresData } = await useFetch<Genre[]>('/api/genres')
 const { data: instrumentData } = await useFetch<Instrument[]>('/api/instruments')
 
-const { data: pendingSubmissions} = await useAsyncData<PendingSubmissionResponse[]>(
+const { data: pendingSubmissions, error} = await useAsyncData<PendingSubmissionResponse[]>(
   'pending-submissions',
     () => api<PendingSubmissionResponse[]>(`/submissions/pending`),
     {
-      default: () => [
-        {
-          id: '1',
-          accountId: 'account_1',
-          accountUsername: 'annanassen',
-          newGenreName: 'Neo Jazz Fusion',
-          startDate: '2025-01-10',
-          endDate: null,
-          description: 'A modern blend of jazz, electronic, and ambient textures.',
-          isSensitive: false,
-          sensitiveDescription: null,
-          playlistLink: 'https://spotify.com',
-          sourceLinks: ['https://wikipedia.com'],
-          aliases: ['Neo Jazz', 'Future Jazz'],
-          countryIds: ['f5a26844-13e1-4a6d-b0b6-001c40be4665', '8512c1ce-d6ee-4190-bf79-04482caa4d75'],
-          instrumentIds: ['cd8958bb-0ece-4602-8437-2d145fc90084'],
-          similarGenreIds: ['b5eaf5eb-5eee-400f-844a-000a73fd52ed', '4d65e956-46c0-4b3e-b070-00547d590679'],
-          subGenreIds: ['982a1419-e87b-4047-b429-0ca8e2c94ea1'],
-          predecessorGenreIds: []
-        },
-        {
-          id: '2',
-          accountId: 'account_2',
-          accountUsername: 'djThit',
-          newGenreName: 'Darkwave Industrial Pop',
-          startDate: '2024-11-03',
-          endDate: null,
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam lectus tempor, consequat magna id, aliquam tortor. Cras eu leo eget nibh bibendum maximus eu id orci. Sed pulvinar diam eros, suscipit bibendum augue facilisis luctus. Praesent auctor mauris in iaculis luctus. Praesent condimentum tellus non mauris pharetra, sed tempus.',
-          isSensitive: true,
-          sensitiveDescription: 'Contains references to dystopian themes and aggressive sound design.',
-          playlistLink: 'https://spotify.com',
-          sourceLinks: [],
-          aliases: ['Dark Pop', 'Industrial Wave'],
-          countryIds: ['a7e452a0-cff9-47d9-9dc6-11ba380d5198'],
-          instrumentIds: ['6bcecefc-7a79-48f0-9c6d-00e36b2d3e80', '2a4b4d71-ead8-4603-9fcc-0edbe9abd1fd'],
-          similarGenreIds: ['4d65e956-46c0-4b3e-b070-00547d590679', 'b3290861-bb97-4e5f-8c85-01958797e917'],
-          subGenreIds: ['699a27d4-bf1f-46ad-a77a-067d8bcdbd8a'],
-          predecessorGenreIds: ['8f006b3a-93ac-4a16-8adf-0af747a40c2f']
-        },
-      ]
+      server: false,
+      default: () => []
+
     }
 )
 
