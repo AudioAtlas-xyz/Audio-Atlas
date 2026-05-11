6ecd7b0357adb6d1315243e42f3784f049e2c73c
Freja <frsj@itu.dk>
2026-05-11 12:38:53 +0200

    Add genre button redirect fix
    Co-authored-by: annanassen <annar@itu.dk>
    Co-authored-by: cfjo <cfjo@itu.dk>

--- frontend/app/components/CountryHeroSection.vue
+++ frontend/app/components/CountryHeroSection.vue
@@ -1,10 +1,36 @@
 <script setup lang="ts">
+import { computed, onMounted, onUnmounted, ref } from 'vue'
+import type { useAuth } from '~/composables/useAuth'
+import LoginModal from "~/components/UserFlow/LoginModal.vue";
+
 const { breadCrumbItems, locationBadges, countryName, description } = defineProps<{
   locationBadges: string[]
   countryName: string
   description: string
   breadCrumbItems: Array<{ label: string, to: string, active?: boolean }>
 }>()
+
+const { user } = useAuth()
+
+// Is the user logged in?
+const isLoggedIn = computed(() => !!user.value)
+
+// Should LoginModal be shown?
+const showLogin = ref(false)
+
+
+const handleButtonClick = () => {
+  if (isLoggedIn.value) {
+    navigateTo('/submission-form')
+  } else {
+    showLogin.value = true
+  }
+}
+
+const closeModal = () => {
+  showLogin.value = false
+}
+
 </script>
 
 <template>
@@ -28,13 +54,20 @@ const { breadCrumbItems, locationBadges, countryName, description } = defineProp
         block
         size="lg"
         color="primary"
-        class="justify-center bg-aurora px-4 py-2 font-medium text-bg hover:bg-aurora rounded-xl"
+        class="justify-center bg-aurora px-4 py-2 font-medium text-bg hover:bg-aurora rounded-xl cursor-pointer"
+        @click="handleButtonClick"
       >
         + Add a genre from {{ countryName }}
       </UButton>
       <p class="text-[11px] text-[#373d5a]">
-        Know a {{ countryName }} genre we&apos;ve missed?
+        Know a {{ countryName }} genre we&apos;ve missed
       </p>
+
+      <div v-if="showLogin" class="overlay">
+        <LoginModal
+        @close="closeModal"
+        ></LoginModal>
+      </div>
     </div>
   </div>
 </template>
